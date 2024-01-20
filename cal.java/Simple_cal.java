857c29c9abe4e6e2804642e05eb72aedebd804f5
prathik <01fe22bca011@kletech.ac.in>
2024-01-20 12:15:17 +0530

    prathik

---
+++ cal.java/Simple_cal.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class cal
+{
+    public float add(int a, int b)
+    {
+        return a + b;
+    }
+    public float sub(int a, int b)
+    {
+        return a - b;
+    }
+    public float mul(int a, int b)
+    {
+        return a * b;
+    }
+    public float div(int a, int b)
+    {
+        return a / b;
+    }
+}
+class Simple_cal
+{
+    public static void main(String args[])
+    {
+        cal c = new cal();
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the number");
+       int exp=sc.nextInt();
+       switch(exp)
+        {
+            case 1:System.out.println("add of two number="+c.add(10,20));
+            break;
+            case 2:System.out.println("sub of two number="+c.sub(20,10));
+            break;
+            case 3:System.out.println("mult of two number="+c.mul(10,20));
+            break;
+            case 4:System.out.println("div of two number="+c.div(10,20));
+        }
+
+    }
+}
